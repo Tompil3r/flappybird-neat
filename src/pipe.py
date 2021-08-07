88383f5154b3b774c374c9284d601e4dd43266c2
Tompil3r <tompil3r@gmail.com>
2021-08-07 19:34:11 +0300

    started Pipe implementation

---
+++ src/pipe.py
@@ -0,0 +1,12 @@
+
+
+class Pipe:
+    '''
+    Pipe is a combination of bottom and top pipes
+    '''
+
+    def __init__(self, top_rect, bottom_rect):
+        self.top_rect = top_rect
+        self.bottom_rect = bottom_rect
+
+        
