33ca8b5e77e9c3fa9dc085f274298296d1610f46
Tompil3r <tompil3r@gmail.com>
2021-09-15 00:29:10 +0300

    added train_neat file

---
+++ train_neat.py
@@ -0,0 +1,20 @@
+# import sys
+# sys.path.append('../')
+from neat import Neat
+from neat.callbacks import GenomeSaving, TimeTermination, EnvStopper, FileLogger
+from flappybird import FlappyBirdEnv
+
+
+population = 200
+
+env = FlappyBirdEnv(population)
+neat = Neat(env.bird_state_len, 1, population)
+
+callbacks = [
+    GenomeSaving(population, best_only=True),
+    TimeTermination(0, 30, 0),
+    EnvStopper(10_000, differentiate_genomes=True, use_scores=False, alive_fitness=10_000),
+    FileLogger('log.csv', population, 5)
+]
+
+neat.fit_env(env, callbacks=callbacks, threads=1, verbose=1, visualize=False)
